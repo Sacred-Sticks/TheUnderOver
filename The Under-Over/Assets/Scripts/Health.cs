6071bd2edc4161ee77265f90551286df167a2ec5
Nathan <nateboy57@yahoo.com>
2022-01-23 02:28:26 -0600

    Jump scare death

--- The Under-Over/Assets/Scripts/Health.cs
+++ The Under-Over/Assets/Scripts/Health.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.AI;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float health;
     [SerializeField] private float damage;
-
+    Camera deathcam;
+    NavMeshAgent monsterAgent;
+    private void Awake()
+    {
+        deathcam = GameObject.Find("deathcam").GetComponent<Camera>();
+        monsterAgent = GameObject.Find("Ceratoferox").GetComponent<NavMeshAgent>();
+    }
     private void OnCollisionEnter(Collision col) {
         if (col.gameObject.tag == "Monster") {
             health -= damage;
@@ -16,7 +22,25 @@ public class Health : MonoBehaviour
 
     private void Update() {
         if (health <= 0) {
-            SceneManager.LoadScene("Culling 2");
+            
+
+            Invoke("DeathRoutine",0f);
+            
         }
+    }
+
+    void DeathRoutine()
+    {
+        // switch camera to close up of monster and change monster animation into killing blow
+        monsterAgent.enabled = false;
+            deathcam.enabled = true;
+        Invoke("WaitToSwap", 4f);
+
+    }
+
+    void WaitToSwap()
+    {
+        Debug.Log("Invoking swap");
+        SceneManager.LoadScene("Culling 2");
     }
 }
